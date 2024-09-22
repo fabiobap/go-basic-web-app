97840901ebe16d15965ff8cd173850b76aa155af
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-09-21 22:58:28 -0300

    session manager

--- config/config.go
+++ config/config.go
@@ -1,9 +1,15 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
 
-//holds application cache
+	"github.com/alexedwards/scs/v2"
+)
+
+// holds application cache
 type AppConfig struct {
 	TemplateCache map[string]*template.Template
 	UseCache      bool
+	InProduction  bool
+	Session       *scs.SessionManager
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -24,13 +24,18 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
 
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
-
 	stringMap["test"] = "Hello from handler"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{StringMap: stringMap})
 }
